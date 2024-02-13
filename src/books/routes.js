aab2b319015413e4d02ce7330a7349d4119d5299
Ethan Rose <ethanrose.er@googlemail.com>
2024-02-13 14:42:18 +0000

    enhancement: changed bookRouter call function

--- src/books/routes.js
+++ src/books/routes.js
@@ -5,4 +5,10 @@ const { addBook } = require("./controllers");
 
 bookRouter.post("/books/addBook", addBook);
 
+// get all books
+
+// update book author
+
+//delete a single book by title
+
 module.exports = bookRouter;
--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(bookRouter());
+app.use(bookRouter);
 
 const syncTables = async () => {
   await Book.sync();
